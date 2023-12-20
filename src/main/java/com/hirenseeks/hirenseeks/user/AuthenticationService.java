73d8e2c46c117cca98ffbfc89f7175f75c1eded6
Jash <jashsharma619@gmail.com>
2023-12-20 14:18:05 +0530

    implimented customResponse model

--- src/main/java/com/hirenseeks/hirenseeks/user/AuthenticationService.java
+++ src/main/java/com/hirenseeks/hirenseeks/user/AuthenticationService.java
@@ -5,6 +5,8 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hirenseeks.hirenseeks.response.CustomResponse;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -14,35 +16,36 @@ public class AuthenticationService {
     private UserRepository userRepository;
     @Autowired
     private UserServiceImpl userService;
+    CustomResponse customResponse = new CustomResponse();
 
     public Map<String, Object> authenticate(
             User userData, HttpServletRequest request) {
-        User userOptional = new User();
+        User mainUser = new User();
         boolean flag = false;
-        Map<String, Object> response = new HashMap<>();
         if (userData.getUserName() != null && userRepository.existsByUserName(userData.getUserName())) {
-            userOptional = userRepository.findUserByUserName(userData.getUserName());
+            mainUser = userRepository.findUserByUserName(userData.getUserName());
             flag = true;
         } else if (userData.getEmail() != null && userRepository.existsByEmail(userData.getEmail())) {
-            userOptional = userRepository.findUserByEmail(userData.getEmail());
+            mainUser = userRepository.findUserByEmail(userData.getEmail());
             flag = true;
         } else if (userData.getContactNumber() != null && userRepository.existsByEmail(userData.getContactNumber())) {
-            userOptional = userRepository.findUserByContactNumber(userData.getContactNumber());
+            mainUser = userRepository.findUserByContactNumber(userData.getContactNumber());
             flag = true;
         } else {
             flag = false;
         }
 
-        if (flag && Encrypt.verifyPassword(userData.getPassword(), userOptional.getPassword())) {
+        if (flag && Encrypt.verifyPassword(userData.getPassword(), mainUser.getPassword())) {
             HttpSession session = request.getSession(true);
-            session.setAttribute("username", userOptional.getUserName());
-            response.put("success", true);
-            userOptional.setPassword(null);
-            response.put("userData", userService.getUsers(userOptional.getUserName()));
+            session.setAttribute("username", mainUser.getUserName());
+
+            mainUser.setLastLogin(new Date());
+            userRepository.save(mainUser);
+            mainUser.setPassword(null);
+
+            return customResponse.returnSuccessTrueResponse("userData", userService.getUsers(mainUser.getUserName()));
         } else {
-            response.put("success", false);
-            response.put("error", "Username or Password Wrong");
+            return customResponse.returnSuccessFalseResponse("Username or Password Wrong");
         }
-        return response;
     }
 }
