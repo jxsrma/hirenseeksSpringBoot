51544bd09bb009888fea6f62a8d35c8005c8e02a
Jash <jashsharma619@gmail.com>
2023-12-20 14:33:21 +0530

    Description comments added

--- src/main/java/com/hirenseeks/hirenseeks/user/UserRepository.java
+++ src/main/java/com/hirenseeks/hirenseeks/user/UserRepository.java
@@ -10,21 +10,58 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.id = ?1")
     User findUserById(Long id);
 
+    /**
+     * Find User by username.
+     *
+     * @param userName Username of the User.
+     * @return User Object form database.
+     */
+    // @throws IllegalArgumentException If either operand is negative.
     @Query("SELECT u FROM User u WHERE u.userName = ?1")
-    User findUserByUserName(String name);
+    User findUserByUserName(String userName);
 
+    /**
+     * Check if the user exists by username.
+     *
+     * @param username Username of the User.
+     * @return true if present else false.
+     */
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.userName = ?1")
     boolean existsByUserName(String username);
 
+    /**
+     * Find User by email.
+     *
+     * @param email Username of the User.
+     * @return User Object form database.
+     */
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findUserByEmail(String email);
 
+    /**
+     * Check if the user exists by email.
+     *
+     * @param email Username of the User.
+     * @return true if present else false.
+     */
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = ?1")
     boolean existsByEmail(String email);
 
+    /**
+     * Find User by contact contactNumber.
+     *
+     * @param contactNumber Username of the User.
+     * @return User Object form database.
+     */
     @Query("SELECT u FROM User u WHERE u.contactNumber = ?1")
     User findUserByContactNumber(String contactNumber);
 
+    /**
+     * Check if the user exists by contactNumber.
+     *
+     * @param contactNumber Username of the User.
+     * @return true if present else false.
+     */
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.contactNumber = ?1")
     boolean existsByContactNumber(String contactNumber);
 
